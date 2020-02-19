3663cb452e404046381d46e32b893275d8ca3e90
Samuel Pritchard <samuel.pritchard@digital.cabinet-office.gov.uk>
2020-02-19 13:11:04 +0000

    Sign and verify, using password

--- global-install/sandbox/hook/notify.py
+++ global-install/sandbox/hook/notify.py
@@ -3,12 +3,16 @@ import subprocess
 import hashlib
 import json
 from base64 import b64encode, b64decode
+
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import utils
 from cryptography.exceptions import InvalidSignature
+import paramiko
+from paramiko.message import Message
+import getpass
 
 
 def repeat_to_length(one_time, length):
@@ -20,31 +24,20 @@ def divider():
 
 
 def private_key_sign(message, private_key):
-    chosen_hash = hashes.SHA256()
-    hasher = hashes.Hash(chosen_hash, default_backend())
-    hasher.update(message.encode('utf-8'))
-    digest = hasher.finalize()
-    sig = private_key.sign(
-        digest,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        utils.Prehashed(chosen_hash)
+    signed_data = private_key.sign(
+        message.encode("utf-8"), padding=padding.PKCS1v15(), algorithm=hashes.SHA256()
     )
-    return b64encode(sig).decode('utf-8')
+    b64_bytes = b64encode(signed_data)
+    b64_data = b64_bytes.decode("utf-8")
+    return b64_data
 
 
 def public_key_verify(signature, message, public_key):
     try:
+        b64_bytes = signature.encode("utf-8")
+        bytes_data = b64decode(b64_bytes)
         public_key.verify(
-            b64decode(signature.encode('utf-8')),
-            message.encode('utf-8'),
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
+            bytes_data, message.encode("utf-8"), padding.PKCS1v15(), hashes.SHA256()
         )
         verified = True
     except InvalidSignature:
@@ -52,16 +45,13 @@ def public_key_verify(signature, message, public_key):
     return verified
 
 
-def get_ssh_key(host='github.com'):
+def get_ssh_key(host="github.com"):
     home = os.environ.get("HOME")
 
     identity_file = f"{home}/.ssh/id_rsa"
 
-    result = subprocess.run(
-        ["cat", f"{home}/.ssh/config"],
-        stdout=subprocess.PIPE
-    )
-    output = result.stdout.decode('utf-8')
+    result = subprocess.run(["cat", f"{home}/.ssh/config"], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
     lines = output.split("\n")
     for linenumber, line in enumerate(lines):
         if host in line:
@@ -78,27 +68,20 @@ def get_ssh_key(host='github.com'):
     return identity_file
 
 
-def load_private_key(key_file):
-    content = read_file(key_file)
-    private_key = serialization.load_pem_private_key(
-        content.encode('utf-8'),
-        password=None,
-        backend=default_backend()
-    )
+def load_private_key(key_file, ssh_password=None):
+    rsa_key = paramiko.RSAKey.from_private_key_file(key_file, ssh_password)
+    private_key = rsa_key.key
     return private_key
 
 
 def load_public_key(key_file):
-    content = read_file(key_file)
-    public_key = serialization.load_ssh_public_key(
-        content.encode('utf-8'),
-        backend=default_backend()
-    )
+    content = read_file(key_file, "rb")
+    public_key = serialization.load_ssh_public_key(content, backend=default_backend())
     return public_key
 
 
-def read_file(file_path):
-    file_handle = open(file_path, "r")
+def read_file(file_path, flags="r"):
+    file_handle = open(file_path, flags)
     contents = file_handle.read()
     file_handle.close()
     return contents
@@ -106,50 +89,43 @@ def read_file(file_path):
 
 def calc_md5(source_string):
     """Implement hashlib to calculate md5 from a given string"""
-    undigested = source_string.encode('utf-8')
+    undigested = source_string.encode("utf-8")
     hashed = hashlib.md5()
     hashed.update(undigested)
     digest = hashed.hexdigest()
     return digest
 
 
-def notify(endpoint, token):
-    post_data = {
-        "commit": {
-            'number': 12524,
-            'type': 'issue',
-            'action': 'show'
-        }
-    }
+def notify(ssh_password=None):
+    token = os.environ.get("TOKEN")
+    endpoint = os.environ.get("ENDPOINT")
+    post_data = {"commit": {"number": 12524, "type": "issue", "action": "show"}}
 
-    post_data['md5'] = calc_md5(json.dumps(post_data['commit']))
+    post_data["md5"] = calc_md5(json.dumps(post_data["commit"]))
 
     ssh_file = get_ssh_key()
     print(ssh_file)
-    private_key = load_private_key(ssh_file)
+    private_key = load_private_key(ssh_file, ssh_password)
     public_key = load_public_key(f"{ssh_file}.pub")
 
-    sign = private_key_sign(post_data['md5'], private_key)
+    sign = private_key_sign(post_data["md5"], private_key)
 
     divider()
     print("Signed")
     print(sign)
     divider()
 
-    verified = public_key_verify(sign, post_data['md5'], public_key)
+    verified = public_key_verify(sign, post_data["md5"], public_key)
 
     divider()
     print("Verified?")
     print(verified)
     divider()
 
-    result = subprocess.run(
-        ['git', 'config', '--global', '-l'],
-        stdout=subprocess.PIPE
-    )
-    output = result.stdout.decode('utf-8')
+    result = subprocess.run(["git", "config", "--global", "-l"], stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
     lines = output.split("\n")
-    wanted = ['user.name', 'user.email']
+    wanted = ["user.name", "user.email"]
     for line in lines:
         terms = line.split("=")
         if len(terms) > 1:
@@ -166,7 +142,7 @@ def notify(endpoint, token):
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
         "Accept": "*/*",
-        "User-Agent": "GitHub/Hook/pre-commit"
+        "User-Agent": "GitHub/Hook/pre-commit",
     }
 
     url = f"https://{endpoint}/?alert_name=pre-commit"
@@ -182,8 +158,7 @@ def notify(endpoint, token):
     # conn.close()
 
 
-if __name__ == '__main__':
-    TOKEN = os.environ.get('TOKEN')
-    ENDPOINT = os.environ.get('ENDPOINT')
+if __name__ == "__main__":
 
-    notify(ENDPOINT, TOKEN)
+    ssh_password = getpass.getpass("Password:").encode()
+    notify(ssh_password)
