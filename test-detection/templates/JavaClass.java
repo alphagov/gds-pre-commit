43b3d51c966e655a0ae322a136c7771d9d17b269
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-02-21 16:39:32 +0000

    Add ruby template and tinker with java template

--- test-detection/templates/JavaClass.java
+++ test-detection/templates/JavaClass.java
@@ -4,7 +4,9 @@ public class JavaClass {
     * This is just a test
     */
 
-   public static void main(String []args) {
-      System.out.println("%SINGLE_LINE_VARIABLE%");
+   public String single_line_variable = "";
+
+   public void main() {
+      this.single_line_variable = "%SINGLE_LINE_VARIABLE%";
    }
 }
---
+++ test-detection/templates/ruby_class.rb
@@ -0,0 +1,5 @@
+class RubyClass
+   def initialize()
+      @single_line_variable = "%SINGLE_LINE_VARIABLE%"
+   end
+end
