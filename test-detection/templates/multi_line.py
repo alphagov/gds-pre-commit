a0e5507afe3e4e780ea66a7030dddcd7b36a9148
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-02-21 13:51:17 +0000

    Remove single python template and separate single line and multi line var tests in separate templates.

--- test-detection/templates/multi_line.py
+++ test-detection/templates/multi_line.py
@@ -1,7 +1,5 @@
 """ Create a placeholder file which can be populated with string variables to test pre-commit hooks """
 
-single_line_variable = "%SINGLE_LINE_VARIABLE%"
-
 multi_line_variable = """
 %MULTI_LINE_VARIABLE%
 """
---
+++ test-detection/templates/single_line.py
@@ -0,0 +1,3 @@
+""" Create a placeholder file which can be populated with string variables to test pre-commit hooks """
+
+single_line_variable = "%SINGLE_LINE_VARIABLE%"
