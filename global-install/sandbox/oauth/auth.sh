0f10731715cf91e7e8aa8a993e87586e19e729bf
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-03-03 15:26:10 +0000

    Switch endpoint with mode flag and add execution permission.

--- global-install/sandbox/oauth/auth.sh
+++ global-install/sandbox/oauth/auth.sh
@@ -43,15 +43,22 @@ if [[ "$1" == '--' ]]; then shift; fi
 if [[ -z "${mode}" ]]; then
   mode="prod"
 fi
-
 tmpfile="/tmp/gh_auth.json"
-if [ ! -f "/tmp/gh_auth.json" ]; then
+
+# Change the submission URL based on the mode argument
+if [[ $mode == "prod" ]]; then
+  endpoint="alert-controller.gds-cyber-security.digital"
+else
+  endpoint="alert-controller.staging.gds-cyber-security.digital"
+fi
+
+if [ ! -f "${tmpfile}" ]; then
   timestamp=$(date)
   post='{"scopes":["read:user", "read:org"],"note":"GDS GitHub Usage Reporting '${timestamp}'"}'
   curl -H "X-GitHub-OTP: ${otp}" -u ${username} -d "${post}" https://api.github.com/authorizations > $tmpfile
 fi
 token=$(cat $tmpfile | jq -r '.token')
-response=$(curl -H "Authorization: github ${token}" -H "User-Agent: GitHub/Hook" -d '{"action":"register"}' https://alert-controller.staging.gds-cyber-security.digital?alert_name=register)
+response=$(curl -H "Authorization: github ${token}" -H "User-Agent: GitHub/Hook" -d '{"action":"register"}' https://${endpoint}?alert_name=register)
 secret=$(echo $response | jq -r '.user_secret')
 username=$(echo $response | jq -r '.username')
 git config --global gds.github-reporting-token "${secret}"
