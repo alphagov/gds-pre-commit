fcc2a1506b9302ec6fc88166a3fddff00c97deb6
Dan Jones <dan.jones@digital.cabinet-office.gov.uk>
2020-03-03 10:41:27 +0000

    Do an oauth user password grant, send registration to alert processor and retrieve username and secret from response.

--- global-install/sandbox/oauth/auth.sh
+++ global-install/sandbox/oauth/auth.sh
@@ -8,13 +8,13 @@ Usage:
 
   source auth.sh \\
     -u github_username \\
-    -c otp-code
+    -c 2fa-code
 
 OR:
 
   source auth.sh \\
     --username github_username \\
-    --otp-code otp-code
+    --code 2fa-code
 
 EOF
 }
@@ -24,7 +24,7 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
   -u | --username )
     shift; username=$1
     ;;
-  -c | --otp-code )
+  -c | --code )
     shift; otp=$1
     ;;
   -h | --help )
@@ -34,8 +34,15 @@ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
 esac; shift; done
 if [[ "$1" == '--' ]]; then shift; fi
 
-timestamp=$(date)
-curl -H "X-GitHub-OTP: ${otp}" -u ${username} -d '{"scopes":["read:user"],"note":"gh_auth ${timestamp}"}' https://api.github.com/authorizations > /tmp/gh_auth.json
-token=$(cat /tmp/gh_auth.json | jq -r '.token')
-curl -H "Authorization: github ${token}" -d '{"action":"register"}' https://alert-controller.staging.gds-cyber-security.digital?alert_name=register
-rm /tmp/gh_auth.json
+tmpfile="/tmp/gh_auth.json"
+if [ ! -f "/tmp/gh_auth.json" ]; then
+  timestamp=$(date)
+  post='{"scopes":["read:user", "read:org"],"note":"gh_auth '${timestamp}'"}'
+  curl -H "X-GitHub-OTP: ${otp}" -u ${username} -d "${post}" https://api.github.com/authorizations > $tmpfile
+fi
+token=$(cat $tmpfile | jq -r '.token')
+response=$(curl -H "Authorization: github ${token}" -H "User-Agent: GitHub/Hook" -d '{"action":"register"}' https://alert-controller.staging.gds-cyber-security.digital?alert_name=register)
+secret=$(echo $response | jq -r '.user_secret')
+username=$(echo $response | jq -r '.username')
+git config --global gds.github-reporting-token "${secret}"
+git config --global gds.github-username "${username}"
