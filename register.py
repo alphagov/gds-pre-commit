0654e81b6a73b49e52becd54de72639e3862b789
Tristram Oaten <tristram@oaten.name>
2020-03-09 11:56:26 +0000

    fix input again
    the `future` package, and therefore the `past` module isn't always installed

--- register.py
+++ register.py
@@ -7,8 +7,8 @@ import sys
 from runner import run
 
 try:
-    from past.builtins import raw_input  # type: ignore
-except ImportError:
+    input = raw_input  # type: ignore
+except NameError:
     pass
 
 
@@ -28,7 +28,7 @@ def register(mode="prod"):
 
     if not username:
         # Prompt user for github username
-        username = raw_input("Enter your github username:")
+        username = input("Enter your github username:")
 
     # OAuth for registration credentials
     token = run("git config --global gds.github-registration-token")
@@ -38,7 +38,7 @@ def register(mode="prod"):
             "This creates a personal access token with read:user and read:org scopes. "
         )
         # Prompt user for 2FA
-        otp = raw_input("Please enter your GitHub 2FA code:")
+        otp = input("Please enter your GitHub 2FA code:")
 
         print(
             "Requesting authorization from GitHub - "
