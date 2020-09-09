8060ca117fd15274a7c7ab96941de3e78563f6f6
Tristram Oaten <tristram@oaten.name>
2020-09-09 11:04:55 +0100

    fixes

--- install.py
+++ install.py
@@ -1,26 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import sys
+
 import os
-
-def detect(subpath, error):  
-    if subpath in sys.executable:
-    print(error)
-    sys.exit(1)
-
-detect("pyenv", "pyenv interpreter detected, please install with your system python")
-detect("virtualenvs", "virtualenvironment interpreter detected, please install with your system python")
+import sys
 
 from register import register
 from runner import run
+
+
+def detect(subpath, error):
+    "if `subpath` found in the executable path, print `error` and exit."
+    if subpath in sys.executable:
+        print(error)
+        sys.exit(1)
+
+
+detect("pyenv", "pyenv interpreter detected, please install with your system python")
+detect(
+    "virtualenvs",
+    "virtualenvironment interpreter detected, please install with your system python",
+)
+
 
 try:
     input = raw_input  # type: ignore
 except NameError:
     pass
 
+
 def hookpath():
+    "Returns the absolute path to the hooks directory"
     p = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(p, "global_install", "hooks")
 
