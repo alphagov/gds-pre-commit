730e5a5f304080f099359dc126eb45c70684d869
Tristram Oaten <tristram@oaten.name>
2020-09-22 17:00:09 +0100

    cleaned up old environment. RIP

--- runner.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def run(cmd):
-    """ """
-    try:
-        return subprocess.check_output(cmd, shell=True).decode("UTF-8").strip()
-    except subprocess.CalledProcessError:
-        return None
